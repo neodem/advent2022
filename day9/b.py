fc601561dd369959f395b21943247980dc40929a
Vincent Fumo <Vincent_Fumo@cable.comcast.com>
2022-12-11 18:25:55 -0500

    adds plotting to b

--- day9/b.py
+++ day9/b.py
@@ -1,6 +1,7 @@
 import common
 import functions
 from common import Point
+from common import Plot
 
 
 def move_links(head_loc, link_locs, direc):
@@ -12,10 +13,16 @@ def move_links(head_loc, link_locs, direc):
         index = index + 1
 
 
-head_location = Point(0, 0, "head")
-link_locations = [Point(0, 0, str(i + 1)) for i in range(9)]
+head_location = Point(0, 0, "head", "H")
+link_locations = [Point(0, 0, str(i + 1), str(i + 1)) for i in range(9)]
 link_locations[8].name = "tail"
+link_locations[8].id = "T"
 tail_visits = set()
+
+p = Plot(40, 40)
+for link_location in link_locations:
+    p.add_location(link_location)
+p.add_location(head_location)
 
 filename = "testb.dat"
 commands = common.read_file_as_lines(filename)
@@ -35,4 +42,6 @@ for command in commands:
         move_links(head_location, link_locations, direc)
         tail_visits.add(link_locations[8])
         print("unique tail visit count: {}".format(len(tail_visits)))
+
+    p.print_plot(1)
     print()

b2847da343ac8d67e4c1632054b9bf6da827c70b
Vincent Fumo <Vincent_Fumo@cable.comcast.com>
2022-12-14 19:48:04 -0500

    more improvements

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # advent2022
 
-This is my Advent of Code repo for 2022.
+This is my Advent of Code repo for 2022. I'm trying to limit the amount of time I spend on each one so the quality of the code isn't great. I'm also not a python programmer.
--- day12/a.py
+++ day12/a.py
@@ -13,24 +13,61 @@ class Node:
     distance_to_end = None
 
     # forward paths point to Nodes
-    f_paths = []
+    f_paths = None
 
     # backward paths point to Nodes
-    b_paths = []
+    b_paths = None
+
+    def __str__(self):
+        return "[{}.{}]".format(self.index, self.value)
 
     def __init__(self, index, value):
         self.index = index
         self.value = value
+        self.f_paths = set()
+        self.b_paths = set()
 
-    def gte(self, other_node):
-        return ord(self.value) >= ord(other_node.value)
+    def __hash__(self):
+        return self.index
+
+    def __eq__(self, other):
+        if isinstance(other, Node):
+            return self.index == other.index
+        return NotImplemented
+
+    def get_clean_value(self):
+        if self.value == 'S':
+            return 'a'
+
+        if self.value == 'E':
+            return 'z'
+
+        return self.value
+
+    def can_step(self, other_node):
+        # we can step to an equal
+        # or one greater
+        # or any less
+
+        our_level = ord(self.get_clean_value())
+        their_level = ord(other_node.get_clean_value())
+
+        if our_level == their_level:
+            return True
+
+        if our_level > their_level:
+            return True
+
+        if our_level + 1 == their_level:
+            return True
 
     def add_back_connection(self, node):
-        self.b_paths.append(node)
+        if not self.f_paths.__contains__(node):
+            self.b_paths.add(node)
 
     def add_connection(self, node):
-        node.add_back_connection(self)
-        self.f_paths.append(node)
+        if not self.b_paths.__contains__(node):
+            self.f_paths.add(node)
 
 
 # class Maze:
@@ -75,7 +112,7 @@ class IndexedMatrix:
         return self.get_by_index(index)
 
     def get_row_from_index(self, index):
-        return index % self.num_cols
+        return index // self.num_cols
 
     def get_col_from_index(self, index):
         r = self.get_row_from_index(index)
@@ -114,27 +151,54 @@ lines = common.read_file_as_lines("test.dat")
 m = IndexedMatrix(lines)
 m.draw_matrix()
 
-start_node = None
+nodes = []
 for index in range(m.max_index()):
     node = m.get_by_index(index)
-    if not start_node:
-        start_node = node
 
-    right = m.get_right(index)
-    if node.gte(right):
-        node.add_connection(right)
+    if node.value != 'E':
+        right = m.get_right(index)
+        if right is not None and node.can_step(right):
+            node.add_connection(right)
+            right.add_back_connection(node)
 
-    left = m.get_left(index)
-    if node.gte(left):
-        node.add_connection(left)
+        left = m.get_left(index)
+        if left is not None and node.can_step(left):
+            node.add_connection(left)
+            left.add_back_connection(node)
 
-    up = m.get_above(index)
-    if node.gte(up):
-        node.add_connection(up)
+        up = m.get_above(index)
+        if up is not None and node.can_step(up):
+            node.add_connection(up)
+            up.add_back_connection(node)
 
-    down = m.get_below(index)
-    if node.gte(down):
-        node.add_connection(down)
+        down = m.get_below(index)
+        if down is not None and node.can_step(down):
+            node.add_connection(down)
+            down.add_back_connection(node)
+
+    nodes.append(node)
+
+
+def find_node(nodes, val):
+    for node in nodes:
+        if node.value == val:
+            return node
+    return None
+
+
+start_node = find_node(nodes, 'S')
+end_node = find_node(nodes, 'E')
+
+
+def set_back_distances(node, distance):
+    back_paths = node.b_paths
+    if back_paths is not None and len(back_paths) > 0:
+        for back_path in back_paths:
+            back_path.distance_to_end = distance
+            set_back_distances(back_path, distance + 1)
+
+
+set_back_distances(end_node, 1)
 
 print(m.get_by_index(0))
 print(m.get_by_index(1))

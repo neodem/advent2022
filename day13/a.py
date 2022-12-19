c8d52dc9c5e4f051815dfcb753b93592768318cd
Vincent Fumo <Vincent_Fumo@cable.comcast.com>
2022-12-19 12:51:03 -0500

    adds logging flag

--- day13/a.py
+++ day13/a.py
@@ -5,7 +5,7 @@ pairs = functions.read_file_to_pairs('test.dat')
 right_order_index_sum = 0
 
 for pair in pairs:
-    pair.compute()
+    pair.compute(logging=True)
     print()
     if pair.right_order:
         right_order_index_sum += pair.index
--- day13/classes.py
+++ day13/classes.py
@@ -68,18 +68,20 @@ class Pair(object):
     def set_line2(self, line):
         self.input2 = self.parse_string_to_lists(line)
 
-    def compute(self):
-        print("== Pair {} ==".format(self.index))
+    def compute(self, logging=False):
+        if logging:
+            print("== Pair {} ==".format(self.index))
 
-        result = self.compare(self.input1, self.input2)
+        result = self.compare(self.input1, self.input2, logging=logging)
         if result >= 0:
             self.right_order = True
         else:
             self.right_order = False
 
-    def compare(self, left, right, indent=0):
-        spaces = " ".join([' ' for _ in range(indent)])
-        print("{}- Compare: {} vs {}".format(spaces, left, right))
+    def compare(self, left, right, indent=0, logging=False):
+        if logging:
+            spaces = " ".join([' ' for _ in range(indent)])
+            print("{}- Compare: {} vs {}".format(spaces, left, right))
 
         # return -1 if left is greater, +1 if right is greater, 0 if the same
 
@@ -87,14 +89,15 @@ class Pair(object):
         if type(left) is int and type(right) is int:
             result = right - left
 
-            if result > 0:
-                indent += 2
-                spaces = " ".join([' ' for _ in range(indent)])
-                print("{}- Left side is smaller, so inputs are in the right order".format(spaces))
-            if result < 0:
-                indent += 2
-                spaces = " ".join([' ' for _ in range(indent)])
-                print("{}- Right side is smaller, so inputs are not in the right order".format(spaces))
+            if logging:
+                if result > 0:
+                    indent += 2
+                    spaces = " ".join([' ' for _ in range(indent)])
+                    print("{}- Left side is smaller, so inputs are in the right order".format(spaces))
+                if result < 0:
+                    indent += 2
+                    spaces = " ".join([' ' for _ in range(indent)])
+                    print("{}- Right side is smaller, so inputs are not in the right order".format(spaces))
 
             return result
 
@@ -102,29 +105,32 @@ class Pair(object):
         if type(left) is int or type(right) is int:
             if type(left) is int:
                 new_left = [left]
-                indent += 2
-                spaces = " ".join([' ' for _ in range(indent)])
-                print("{}- Mixed types; convert left to {} and retry comparison".format(spaces, new_left))
-                return self.compare(new_left, right, indent=indent)
+                if logging:
+                    indent += 2
+                    spaces = " ".join([' ' for _ in range(indent)])
+                    print("{}- Mixed types; convert left to {} and retry comparison".format(spaces, new_left))
+                return self.compare(new_left, right, indent=indent, logging=logging)
 
             new_right = [right]
-            indent += 2
-            spaces = " ".join([' ' for _ in range(indent)])
-            print("{}- Mixed types; convert right to {} and retry comparison".format(spaces, new_right))
-            return self.compare(left, new_right, indent=indent)
+            if logging:
+                indent += 2
+                spaces = " ".join([' ' for _ in range(indent)])
+                print("{}- Mixed types; convert right to {} and retry comparison".format(spaces, new_right))
+            return self.compare(left, new_right, indent=indent, logging=logging)
 
         # if we are here, both left and right are lists we iterate and compare values
         # type(left) is list and type(right) is list:
         while len(left) > 0:
             if len(right) == 0:
                 # right has run out of items
-                indent += 2
-                spaces = " ".join([' ' for _ in range(indent)])
-                print("{}- Right side ran out of items, so inputs are not in the right order".format(spaces))
+                if logging:
+                    indent += 2
+                    spaces = " ".join([' ' for _ in range(indent)])
+                    print("{}- Right side ran out of items, so inputs are not in the right order".format(spaces))
                 return -1
             left_val = left.pop(0)
             right_val = right.pop(0)
-            result = self.compare(left_val, right_val, indent=indent + 2)
+            result = self.compare(left_val, right_val, indent=indent + 2, logging=logging)
             if result != 0:
                 return result
 
